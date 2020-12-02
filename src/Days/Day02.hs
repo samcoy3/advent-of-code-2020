a31d2dcccb42782101e70732a53afb9586e4c1a2
Sam Coy <samcoy3@gmail.com>
2020-12-02 09:26:13 +0000

    Day 2 solutions

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -1,19 +1,12 @@
 module Days.Day02 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
+import Data.Bits (xor)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +14,45 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = ruleAndPassword `sepBy` endOfLine
+  where
+    ruleAndPassword = do
+      min <- decimal
+      char '-'
+      max <- decimal
+      skipSpace
+      given <- letter
+      asciiCI ": "
+      password <- many1 letter
+      return (PasswordRule {min = min, max = max, givenLetter = given}, password)
 
 ------------ TYPES ------------
-type Input = Void
+type Password = String
 
-type OutputA = Void
+data PasswordRule = PasswordRule
+  { min :: Int,
+    max :: Int,
+    givenLetter :: Char
+  }
+  deriving (Show)
 
-type OutputB = Void
+type Input = [(PasswordRule, Password)]
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = length . filter scorePassword
+  where
+    scorePassword (PasswordRule {..}, password) =
+      let countOfGiven = Map.findWithDefault 0 givenLetter (U.freq password)
+       in countOfGiven >= min && countOfGiven <= max
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = length . filter scorePassword
+  where
+    scorePassword (PasswordRule {..}, password) =
+      (password !! (min -1) == givenLetter) `xor` (password !! (max -1) == givenLetter)
