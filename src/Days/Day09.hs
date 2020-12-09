150e61a69b70c4804d69619ca0bb47dd9a774df0
Sam Coy <samcoy3@gmail.com>
2020-12-09 08:49:23 +0000

    Day 9 solutions

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -1,19 +1,11 @@
 module Days.Day09 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.List as L
 import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +13,38 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA =
+  (!! 25)
+    . fromJust
+    . find
+      ( \ls ->
+          ls !! 25
+            `notElem` [ x + y
+                        | x <- (L.take 25 ls),
+                          y <- (L.take 25 ls),
+                          x /= y
+                      ]
+      )
+    . tails
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input =
+  let target = partA input
+   in (\ls -> minimum ls + maximum ls)
+        . fromJust
+        . find (\ls -> sum ls == target)
+        . concatMap tails
+        . inits
+        $ input
