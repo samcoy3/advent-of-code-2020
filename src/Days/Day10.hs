c71aa667370e37fa317ed069a7509d2f9eb12da2
Sam Coy <samcoy3@gmail.com>
2020-12-10 09:38:26 +0000

    Day 10 solution

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -1,19 +1,12 @@
-module Days.Day10 (runDay) where
+module Days.Day10 (runDay, combinationsOfDiffList) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
+
+import Util.Util
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +14,60 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+-- Sorts the input list and counts the differences of length 1 and 3.
+-- We add one to the total differences of length 3, because the question specifies this.
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input =
+  let diffList =
+        (\ls -> zipWith (flip (-)) (0 : ls) ls)
+          . sort
+          $ input
+   in ((+ 1) . length . filter (== 3) $ diffList) * (length . filter (== 1) $ diffList)
 
 ------------ PART B ------------
+{-
+Counts the number of ways in which a list consisting of 1s and 2s can be "grouped", into a list containing 1s, 2s, and 3s.
+The function works recursively in the following way:
+ - Either we "add" the first two values together, or we don't.
+ - If we don't, then the number of arrangements is equal to the number of arrangements in the list with the first element removed.
+ - (Note that we can only add the first two terms together if their sum is <= 3.)
+ - If we do add the first two together, then the number of ways of arranging the list is the number of arrangements of the list from the third element onwards, with the sum of the first pair prepended.
+ - Finally, note that these two cases (adding the first pair, not adding them) are mutually exclusive; they must result in different groupings. Therefore we add them, rather than multiply them.
+-}
+combinationsOfDiffList :: [Int] -> Int
+combinationsOfDiffList ls = case ls of
+  [] -> 1
+  _ : [] -> 1
+  l : l' : ls' ->
+    combinationsOfDiffList (l' : ls')
+      + ( if l + l' <= 3
+            then combinationsOfDiffList ((l + l') : ls')
+            else 0
+        )
+
+{-
+We construct our difference list as before.
+This time, however, we group it into sublists of elements less than three.
+Why? Well, we're counting the number of possible selections of the adapters, and when the difference is three we are forced to pick both adapters on each side of this difference anyway.
+On the sublists composed of elements less than three, we count the number of ways of "partially combining" adjacent pairs of this list so that all elements in the list remain at most three.
+The number of ways of performing such "partial combinations" is the same as the number of ways of omitting adapters such that all adapters have differences of less than three from each other (proof is left to the reader).
+Finally, since our choice of which adapters we skip in each of these "sublists" is independent of each other, we multiply together the number of combinations in each sublist to get the total number of adapter selections.
+-}
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input =
+  let diffList =
+        chunksByPredicate (/= 3)
+          . (\ls -> zipWith (flip (-)) (0 : ls) ls)
+          . sort
+          $ input
+   in product $ fmap combinationsOfDiffList diffList
