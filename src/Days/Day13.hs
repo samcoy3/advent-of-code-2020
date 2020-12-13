24ed56a4af26f0dc8ac4cfe87ab64c0d2c3a5adc
Sam Coy <samcoy3@gmail.com>
2020-12-13 11:43:34 +0000

    Day 13 solutions

--- src/Days/Day13.hs
+++ src/Days/Day13.hs
@@ -2,18 +2,13 @@ module Days.Day13 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
 import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
+import Data.Functor
+import Data.Function (on)
+import Control.Applicative ((<|>))
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +16,52 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser =
+  (,)
+    <$> decimal <* endOfLine
+    <*> (char 'x' $> Nothing <|> Just <$> decimal) `sepBy` char ','
 
 ------------ TYPES ------------
-type Input = Void
+type Input = (Int, [Maybe Int])
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA (time, ids) =
+  (\idPair -> fst idPair * snd idPair)
+    . minimumBy (compare `on` snd)
+    . fmap (\id -> (id, id - (time `mod` id)))
+    . catMaybes
+    $ ids
 
 ------------ PART B ------------
+-- https://en.wikipedia.org/wiki/Chinese_remainder_theorem#Search_by_sieving
+-- An implementation of the above algorithm
+chineseSieve :: [(Int, Int)] -> Int
+chineseSieve congruences =
+  let (firstA, firstN) = head congruences
+   in chineseSieve' firstA firstN (tail congruences)
+  where
+    chineseSieve' a _ [] = a
+    chineseSieve' a n ((a', n') : congs) =
+      let nextA =
+            fromJust $
+              find ((== a') . (`mod` n')) [a, a + n ..]
+       in chineseSieve' nextA (n * n') congs
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB (_, ids) =
+  let congruences =
+        -- We reverse and sort because the algorithm is slightly faster if we do
+        reverse
+          . sortBy (compare `on` snd)
+          -- Need to do this subtraction because we're given the negative remainder
+          . fmap (\(a, n) -> ((n - a) `mod` n, n))
+          . catMaybes
+          -- This turns (x, Just y) into Just (x, y); and turns (x, Nothing) into Nothing
+          . fmap sequence
+          $ zip [0 ..] ids
+   in chineseSieve congruences
