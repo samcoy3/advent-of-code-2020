a82b3f8f78dd9f62e0095134f84f3624d32be7ce
Sam Coy <samcoy3@gmail.com>
2020-12-14 10:12:14 +0000

    Removed unused imports for day 14

--- src/Days/Day14.hs
+++ src/Days/Day14.hs
@@ -2,20 +2,13 @@ module Days.Day14 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List as L
-import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 import Data.Functor (($>))
-import Debug.Trace
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
