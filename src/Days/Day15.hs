5f462000f0f3fbcf99a4d54a65f7103a79b5da37
Sam Coy <samcoy3@gmail.com>
2020-12-15 17:48:45 +0000

    Slightly improved quality of day 15 solution

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -8,8 +8,10 @@ import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 
 import Control.Monad.State
-import Data.STRef
 import Control.Monad.ST
+
+import qualified Data.HashTable.ST.Cuckoo as HT
+import System.IO.Unsafe
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -27,38 +29,30 @@ type OutputA = Int
 type OutputB = Int
 
 ------------ PART A ------------
--- A lazy, infinite list approach - this is good enough for Part A
-infiniteGameList :: [Int] -> [Int]
-infiniteGameList startingNumbers =
-  let initials = init startingNumbers
-      startingMap = foldl' (\m (p, v) -> Map.insert v p m) Map.empty (zip [0 ..] initials)
-   in (initials ++) $
-        unfoldr
-          ( \(currentEntry, map, index) ->
-              let nextEntry = case map Map.!? currentEntry of
-                    Nothing -> 0
-                    Just e -> index - e
-                  newMap = Map.insert currentEntry index map
-               in Just (currentEntry, (nextEntry, newMap, index + 1))
-          )
-          (last startingNumbers, startingMap, length initials)
-
-partA :: Input -> OutputA
-partA input = (infiniteGameList input) !! (2020 - 1)
-
------------- PART B ------------
--- A version of the above, basically, but in the ST monad.
--- This takes 5 (!) minutes, so I plan to rework it soon, possibly using the `hashtables` package
-partB :: Input -> OutputB
-partB input = runST $ do
-  map <- newSTRef (foldl' (\map (pos, val) -> Map.insert val pos map) Map.empty (zip [1 ..] $ init input))
-  let rounds = [length input + 1 .. 30_000_000]
-  (\roundAction -> foldM roundAction (last input) rounds) $
-    ( \prevVal index -> do
-        currentMap <- readSTRef map
-        ( return $ case currentMap Map.!? prevVal of
+-- An implementation of the memory game in the ST monad, using the hashtables package
+playGame :: [Int] -> Int -> Int
+playGame startingNumbers target = runST $ do
+  -- We allocate a hashtable with 5 million buckets. This is roughly enough.
+  prevsMap <- HT.newSized 5_000_000
+  -- Sequence over the input to initialise our hash table.
+  sequence_ $
+    fmap (\(p, v) -> HT.insert prevsMap v p) (zip [1 ..] $ init startingNumbers)
+  -- Monadically fold over thirty million iterations.
+  -- This is, well, slow. It takes about 2 and a half minutes on my machine.
+  -- Not sure a better implementation in Haskell is possible.
+  let rounds = [length startingNumbers + 1 .. target]
+  let performRound prevVal index = do
+        prevOccurence <- HT.lookup prevsMap prevVal
+        ( return $ case prevOccurence of
             Nothing -> 0
             Just e -> (index - e - 1)
           )
-          <* modifySTRef' map (Map.insert prevVal (index - 1))
-    )
+          <* HT.insert prevsMap prevVal (index - 1)
+  foldM performRound (last startingNumbers) rounds
+
+partA :: Input -> OutputA
+partA input = playGame input 2020
+
+------------ PART B ------------
+partB :: Input -> OutputB
+partB input = playGame input 30_000_000
