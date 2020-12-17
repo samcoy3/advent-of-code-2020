39a5c9b0a105f0c867a29c4df75ac04888093035
Sam Coy <samcoy3@gmail.com>
2020-12-17 11:19:46 +0000

    Day 17 solutions

--- src/Days/Day17.hs
+++ src/Days/Day17.hs
@@ -1,19 +1,15 @@
 module Days.Day17 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
+
+import Util.Parsers
+import Util.Util
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +17,82 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = Set.fromList . Map.keys <$> coordinateParser mapper 0
+  where
+    mapper '#' = Just True
+    mapper _ = Nothing
 
 ------------ TYPES ------------
-type Input = Void
+-- A typeclass to unify the solutions to both parts
+class (Eq p, Ord p) => Point p where
+  -- This is used to lift a point from the input into the relevant space
+  lift :: (Int, Int) -> p
+  -- This is the neighbour function for the relevant point type
+  neighbours :: p -> [p]
+  -- Given the set of active points, this calculates the set of points that we need to think about for the next round
+  consideration :: Set p -> Set p
 
-type OutputA = Void
+type Slice = Set (Int, Int)
 
-type OutputB = Void
+type Input = Slice
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+newtype Point3 = Point3 (Int, Int, Int)
+  deriving (Show, Eq, Ord)
+
+instance Point Point3 where
+  lift (x, y) = Point3 (x, y, 0)
+  consideration activePoints =
+    Set.fromList . Map.keys . Map.filter (>= 2) . freq . Set.foldr (++) [] . Set.map neighbours $ activePoints
+  neighbours (Point3 (x, y, z)) =
+    Point3
+      <$> [ (x', y', z')
+            | x' <- [x -1 .. x + 1],
+              y' <- [y -1 .. y + 1],
+              z' <- [z -1 .. z + 1],
+              x' /= x || y' /= y || z' /= z
+          ]
+
+-- Given the set of currently active points, determines whether a point becomes active or not
+evolveSquare :: (Point p) => Set p -> p -> Bool
+evolveSquare space s =
+  let aliveNeighbours = length $ filter (`elem` space) (neighbours s)
+   in if
+          | s `elem` space && (aliveNeighbours == 2 || aliveNeighbours == 3) ->
+            True
+          | s `notElem` space && (aliveNeighbours == 3) ->
+            True
+          | otherwise -> False
+
+-- Evolves a set of active points, applying the rules simultaneously
+evolveSpace :: (Point p) => Set p -> Set p
+evolveSpace space =
+  Set.filter (evolveSquare space) (consideration space)
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = Set.size . foldr1 (.) (replicate 6 evolveSpace) . Set.map (lift @Point3)
 
 ------------ PART B ------------
+newtype Point4 = Point4 (Int, Int, Int, Int)
+  deriving (Eq, Show, Ord)
+
+instance Point Point4 where
+  lift (x, y) = Point4 (x, y, 0, 0)
+  consideration activePoints =
+    Set.fromList . Map.keys . Map.filter (>= 2) . freq . Set.foldr (++) [] . Set.map neighbours $ activePoints
+  neighbours (Point4 (x, y, z, w)) =
+    Point4
+      <$> [ (x', y', z', w')
+            | x' <- [x -1 .. x + 1],
+              y' <- [y -1 .. y + 1],
+              z' <- [z -1 .. z + 1],
+              w' <- [w -1 .. w + 1],
+              x' /= x || y' /= y || z' /= z || w' /= w
+          ]
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = Set.size . foldr1 (.) (replicate 6 evolveSpace) . Set.map (lift @Point4)
