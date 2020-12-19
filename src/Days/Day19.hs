5a2fa47b8fe8d6497c8640ee8af1d887e641602f
Sam Coy <samcoy3@gmail.com>
2020-12-19 21:27:17 +0000

    Day 19 Part B

--- src/Days/Day19.hs
+++ src/Days/Day19.hs
@@ -15,7 +15,7 @@ import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
 
-import Control.Applicative.Combinators ((<|>))
+import Control.Applicative.Combinators ((<|>), between)
 import Util.Parsers (around)
 import Control.Monad
 import Debug.Trace (traceShow, traceShowId, trace)
@@ -32,16 +32,18 @@ inputParser = (,) <$> (rules <* skipSpace) <*> strings
       index <- decimal
       string ": "
       subRules <-
-        (Character <$> (char '"' >> letter <* char '"'))
-          <|> (SubRules <$> (eitherP (decimal `around` (char ' ')) decimal) `sepBy` string " | ")
+        (Character <$> (between (char '"') (char '"') letter))
+          <|> (SubRules <$> (decimal `sepBy` char ' ') `sepBy` string " | ")
       return (index, subRules)
     rules = Map.fromList <$> rule `sepBy` endOfLine
     strings = many1 letter `sepBy` endOfLine
 
 ------------ TYPES ------------
+-- A rule is either a character (the "leaf");
+-- or it's a list of series of rules
 data Rule
   = Character Char
-  | SubRules [Either (Int, Int) Int]
+  | SubRules [[Int]]
   deriving (Eq, Show)
 
 type RuleMap = Map Int Rule
@@ -50,39 +52,55 @@ type Input = (RuleMap, [String])
 
 type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
-traceShowIdWithContext :: (Show a, Show b) => a -> b -> b
-traceShowIdWithContext context result = trace (show context ++ "\t" ++ show result) result
+-- Tests if a string matches a rule with a given index
+doesMatchRule :: RuleMap -> Int -> String -> Bool
+doesMatchRule map index str =
+  let rule = map Map.! index
+   in case rule of
+        Character c -> pure c == str
+        -- We test here if any of the choice of rules is matched
+        SubRules rs -> any (doesMatchRule' str) rs
+          where
+            -- This function tests whether a given series of rules matches a string
+            -- It uses a recursive approach, breaking the string up smartly at each stage
+            doesMatchRule' str' (r1 : rs) =
+              any (\(s1, s2) -> doesMatchRule map r1 s1 && (doesMatchRule' s2) rs) $ allPossibleBreaks r1 str'
+            doesMatchRule' "" [] = True
+            doesMatchRule' _ _ = False
+            -- We split a string based on the leftmost rule: we only split it in places such that the leftmost rule can generate a string of the given length
+            allPossibleBreaks r s =
+              zipWith splitAt (lengths map (length s) r) (repeat s)
 
-doesMatchRule :: RuleMap -> Rule -> String -> Bool
-doesMatchRule map rule str =
-  -- traceShowIdWithContext (rule, str) $
-  case rule of
-    Character c -> pure c == str
-    SubRules rs -> any doesMatchRule' rs
-      where
-        doesMatchRule' :: Either (Int, Int) Int -> Bool
-        doesMatchRule' (Left (r1, r2)) =
-          any (\(s1, s2) -> doesMatchRule map (map Map.! r1) s1 && doesMatchRule map (map Map.! r2) s2) $ allPossibleBreaks (map Map.! r1) str
-        doesMatchRule' (Right r) = doesMatchRule map (map Map.! r) str
-        allPossibleBreaks :: Rule -> String -> [(String, String)]
-        allPossibleBreaks r s =
-          -- traceShowIdWithContext (rs, r, s) $
-          zipWith splitAt (lengths map r) (repeat str)
-
-lengths :: RuleMap -> Rule -> [Int]
-lengths _ (Character _) = [1]
-lengths map (SubRules rs) = foldr (++) [] (fmap length' rs)
-  where
-    length' :: Either (Int, Int) Int -> [Int]
-    length' (Left (r1, r2)) = nub $ (+) <$> (lengths map (map Map.! r1)) <*> (lengths map (map Map.! r2))
-    length' (Right r) = lengths map (map Map.! r)
+-- Calculates the possible lengths of strings which satisfy a given rule
+-- WARNING: This probably won't work on the general case, but it does work if you only have one "recursive" path
+-- We take a "cap" so that in the case where the list could be infinite, it's capped at a value
+lengths :: RuleMap -> Int -> Int -> [Int]
+lengths map cap r =
+  let rule = map Map.! r
+   in nub $ case rule of
+        Character _ -> [1]
+        SubRules rs -> foldr (++) [] (fmap lengths' rs)
+          where
+            -- lengths' calculates the lengths of strings which satisfy a given series of rules
+            lengths' :: [Int] -> [Int]
+            lengths' rss@(r' : rs) =
+              -- This is the dodgy line: I don't think that the then clause will always work, but because our input is nice, it does
+              if r `elem` rss
+                then [0, (head $ lengths' (delete r rss)) .. cap]
+                else nub $ (+) <$> (lengths map cap r') <*> (lengths' rs)
+            lengths' [] = [0]
 
 partA :: Input -> OutputA
-partA (ruleMap, strings) = length . filter (doesMatchRule ruleMap (ruleMap Map.! 0)) $ strings
+partA (ruleMap, strings) = length . filter (doesMatchRule ruleMap 0) $ strings
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB (ruleMap, strings) = length . filter (doesMatchRule ruleMap' 0) $ strings
+  where
+    ruleMap' =
+      Map.insert 8 (SubRules [[42], [42, 8]])
+        . Map.insert 11 (SubRules [[42, 31], [42, 11, 31]])
+        $ ruleMap
