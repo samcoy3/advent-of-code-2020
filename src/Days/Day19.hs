abf59686e4046237d797519ea86d4cf8f9ac8676
Sam Coy <samcoy3@gmail.com>
2020-12-19 12:13:19 +0000

    Day 19 part A

--- src/Days/Day19.hs
+++ src/Days/Day19.hs
@@ -14,6 +14,11 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+
+import Control.Applicative.Combinators ((<|>))
+import Util.Parsers (around)
+import Control.Monad
+import Debug.Trace (traceShow, traceShowId, trace)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,18 +26,62 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (,) <$> (rules <* skipSpace) <*> strings
+  where
+    rule = do
+      index <- decimal
+      string ": "
+      subRules <-
+        (Character <$> (char '"' >> letter <* char '"'))
+          <|> (SubRules <$> (eitherP (decimal `around` (char ' ')) decimal) `sepBy` string " | ")
+      return (index, subRules)
+    rules = Map.fromList <$> rule `sepBy` endOfLine
+    strings = many1 letter `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+data Rule
+  = Character Char
+  | SubRules [Either (Int, Int) Int]
+  deriving (Eq, Show)
 
-type OutputA = Void
+type RuleMap = Map Int Rule
+
+type Input = (RuleMap, [String])
+
+type OutputA = Int
 
 type OutputB = Void
 
 ------------ PART A ------------
+traceShowIdWithContext :: (Show a, Show b) => a -> b -> b
+traceShowIdWithContext context result = trace (show context ++ "\t" ++ show result) result
+
+doesMatchRule :: RuleMap -> Rule -> String -> Bool
+doesMatchRule map rule str =
+  -- traceShowIdWithContext (rule, str) $
+  case rule of
+    Character c -> pure c == str
+    SubRules rs -> any doesMatchRule' rs
+      where
+        doesMatchRule' :: Either (Int, Int) Int -> Bool
+        doesMatchRule' (Left (r1, r2)) =
+          any (\(s1, s2) -> doesMatchRule map (map Map.! r1) s1 && doesMatchRule map (map Map.! r2) s2) $ allPossibleBreaks (map Map.! r1) str
+        doesMatchRule' (Right r) = doesMatchRule map (map Map.! r) str
+        allPossibleBreaks :: Rule -> String -> [(String, String)]
+        allPossibleBreaks r s =
+          -- traceShowIdWithContext (rs, r, s) $
+          zipWith splitAt (lengths map r) (repeat str)
+
+lengths :: RuleMap -> Rule -> [Int]
+lengths _ (Character _) = [1]
+lengths map (SubRules rs) = foldr (++) [] (fmap length' rs)
+  where
+    length' :: Either (Int, Int) Int -> [Int]
+    length' (Left (r1, r2)) = nub $ (+) <$> (lengths map (map Map.! r1)) <*> (lengths map (map Map.! r2))
+    length' (Right r) = lengths map (map Map.! r)
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA (ruleMap, strings) = length . filter (doesMatchRule ruleMap (ruleMap Map.! 0)) $ strings
 
 ------------ PART B ------------
 partB :: Input -> OutputB
